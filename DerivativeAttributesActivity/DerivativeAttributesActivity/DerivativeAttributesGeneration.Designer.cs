5e8bbef2e59b29a8649db131bc3b97da35ff81bf
Pizzayolo <dvizzari92@gmail.com>
2018-08-01 10:32:29 +0000

    Initial commit

---
+++ DerivativeAttributesActivity/DerivativeAttributesActivity/DerivativeAttributesGeneration.Designer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Reflection;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace FIM.DerivativeAttributesActivity
+{
+    public partial class DerivativeAttributesGeneration
+    {
+        #region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        [System.Diagnostics.DebuggerNonUserCode]
+        [System.CodeDom.Compiler.GeneratedCode("", "")]
+        private void InitializeComponent()
+        {
+            this.CanModifyActivities = true;
+            System.Workflow.ComponentModel.ActivityBind activitybind1 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind2 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind3 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind4 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind5 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind6 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind7 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind8 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind9 = new System.Workflow.ComponentModel.ActivityBind();
+            System.Workflow.ComponentModel.ActivityBind activitybind10 = new System.Workflow.ComponentModel.ActivityBind();
+            this.updateResourceActivity1 = new Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity();
+            this.InitializeUpdate = new System.Workflow.Activities.CodeActivity();
+            this.readResourceActivity1 = new Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity();
+            this.InitializeReadSubject = new System.Workflow.Activities.CodeActivity();
+            this.ReadCurrentRequestActivity = new Microsoft.ResourceManagement.Workflow.Activities.CurrentRequestActivity();
+            // 
+            // updateResourceActivity1
+            // 
+            activitybind1.Name = "DerivativeAttributesGeneration";
+            activitybind1.Path = "updateResourceActivity1_ActorId1";
+            activitybind2.Name = "DerivativeAttributesGeneration";
+            activitybind2.Path = "updateResourceActivity1_ApplyAuthorizationPolicy1";
+            activitybind3.Name = "DerivativeAttributesGeneration";
+            activitybind3.Path = "updateResourceActivity1_AuthorizationWaitTimeInSeconds1";
+            this.updateResourceActivity1.Name = "updateResourceActivity1";
+            activitybind4.Name = "DerivativeAttributesGeneration";
+            activitybind4.Path = "updateResourceActivity1_ResourceId1";
+            activitybind5.Name = "DerivativeAttributesGeneration";
+            activitybind5.Path = "updateResourceActivity1_UpdateParameters1";
+            this.updateResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity.ActorIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind1)));
+            this.updateResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity.ApplyAuthorizationPolicyProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind2)));
+            this.updateResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity.ResourceIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind4)));
+            this.updateResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity.UpdateParametersProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind5)));
+            this.updateResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity.AuthorizationWaitTimeInSecondsProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind3)));
+            // 
+            // InitializeUpdate
+            // 
+            this.InitializeUpdate.Name = "InitializeUpdate";
+            this.InitializeUpdate.ExecuteCode += new System.EventHandler(this.InitializeUpdate_ExecuteCode);
+            // 
+            // readResourceActivity1
+            // 
+            activitybind6.Name = "DerivativeAttributesGeneration";
+            activitybind6.Path = "readResourceActivity1_ActorId1";
+            this.readResourceActivity1.Name = "readResourceActivity1";
+            activitybind7.Name = "DerivativeAttributesGeneration";
+            activitybind7.Path = "readResourceActivity1_Resource1";
+            activitybind8.Name = "DerivativeAttributesGeneration";
+            activitybind8.Path = "readResourceActivity1_ResourceId1";
+            activitybind9.Name = "DerivativeAttributesGeneration";
+            activitybind9.Path = "readResourceActivity1_SelectionAttributes1";
+            this.readResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.ActorIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind6)));
+            this.readResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.ResourceProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind7)));
+            this.readResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.ResourceIdProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind8)));
+            this.readResourceActivity1.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity.SelectionAttributesProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind9)));
+            // 
+            // InitializeReadSubject
+            // 
+            this.InitializeReadSubject.Name = "InitializeReadSubject";
+            this.InitializeReadSubject.ExecuteCode += new System.EventHandler(this.InitiliazeReadSubject_ExecuteCode);
+            // 
+            // ReadCurrentRequestActivity
+            // 
+            activitybind10.Name = "DerivativeAttributesGeneration";
+            activitybind10.Path = "currentRequestActivity1_CurrentRequest1";
+            this.ReadCurrentRequestActivity.Name = "ReadCurrentRequestActivity";
+            this.ReadCurrentRequestActivity.SetBinding(Microsoft.ResourceManagement.Workflow.Activities.CurrentRequestActivity.CurrentRequestProperty, ((System.Workflow.ComponentModel.ActivityBind)(activitybind10)));
+            // 
+            // DerivativeAttributesGeneration
+            // 
+            this.Activities.Add(this.ReadCurrentRequestActivity);
+            this.Activities.Add(this.InitializeReadSubject);
+            this.Activities.Add(this.readResourceActivity1);
+            this.Activities.Add(this.InitializeUpdate);
+            this.Activities.Add(this.updateResourceActivity1);
+            this.Name = "DerivativeAttributesGeneration";
+            this.CanModifyActivities = false;
+
+        }
+
+        #endregion
+
+        private Microsoft.ResourceManagement.Workflow.Activities.UpdateResourceActivity updateResourceActivity1;
+
+        private CodeActivity InitializeUpdate;
+
+        private Microsoft.ResourceManagement.Workflow.Activities.ReadResourceActivity readResourceActivity1;
+
+        private CodeActivity InitializeReadSubject;
+
+        private Microsoft.ResourceManagement.Workflow.Activities.CurrentRequestActivity ReadCurrentRequestActivity;
+
+
+
+
+
+
+
+    }
+}
---
+++ DerivativeAttributesActivity/DerivativeAttributesActivity/DerivativeAttributesGeneration.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Linq;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+using Microsoft.ResourceManagement.WebServices.WSResourceManagement;
+using Microsoft.ResourceManagement.Workflow.Activities;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.DirectoryServices;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace FIM.DerivativeAttributesActivity
+{
+    public partial class DerivativeAttributesGeneration : SequenceActivity
+    {
+        private Guid requestorGUID;
+        private Guid targetGUID;
+        private string displayName;
+        private string samAccountName;
+        const string FIMADMGUID = "4074f258-a177-42df-b8ca-cba1a0cefb25";
+
+        #region ReadCurrentRequest
+        public static DependencyProperty currentRequestActivity1_CurrentRequest1Property = DependencyProperty.Register
+            ("currentRequestActivity1_CurrentRequest1", typeof(Microsoft.ResourceManagement.WebServices.WSResourceManagement.RequestType),
+            typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Misc")]
+        public Microsoft.ResourceManagement.WebServices.WSResourceManagement.RequestType currentRequestActivity1_CurrentRequest1
+        {
+            get
+            {
+                return ((Microsoft.ResourceManagement.WebServices.WSResourceManagement.RequestType)(base.GetValue
+                    (FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.currentRequestActivity1_CurrentRequest1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.currentRequestActivity1_CurrentRequest1Property, value);
+            }
+        }
+        #endregion
+        #region ReadResourceActivity
+        public static DependencyProperty readResourceActivity1_ActorId1Property = DependencyProperty.Register("readResourceActivity1_ActorId1", typeof(System.Guid), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Guid readResourceActivity1_ActorId1
+        {
+            get
+            {
+                return ((System.Guid)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_ActorId1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_ActorId1Property, value);
+            }
+        }
+
+        public static DependencyProperty readResourceActivity1_Resource1Property = DependencyProperty.Register("readResourceActivity1_Resource1", typeof(Microsoft.ResourceManagement.WebServices.WSResourceManagement.ResourceType), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public ResourceType readResourceActivity1_Resource1
+        {
+            get
+            {
+                return ((Microsoft.ResourceManagement.WebServices.WSResourceManagement.ResourceType)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_Resource1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_Resource1Property, value);
+            }
+        }
+
+        public static DependencyProperty readResourceActivity1_ResourceId1Property = DependencyProperty.Register("readResourceActivity1_ResourceId1", typeof(System.Guid), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Guid readResourceActivity1_ResourceId1
+        {
+            get
+            {
+                return ((System.Guid)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_ResourceId1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_ResourceId1Property, value);
+            }
+        }
+
+        public static DependencyProperty readResourceActivity1_SelectionAttributes1Property = DependencyProperty.Register("readResourceActivity1_SelectionAttributes1", typeof(System.String[]), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public String[] readResourceActivity1_SelectionAttributes1
+        {
+            get
+            {
+                return ((string[])(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_SelectionAttributes1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.readResourceActivity1_SelectionAttributes1Property, value);
+            }
+        }
+        #endregion
+        #region UpdateResource
+        public static DependencyProperty updateResourceActivity1_ActorId1Property = DependencyProperty.Register("updateResourceActivity1_ActorId1", typeof(System.Guid), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Guid updateResourceActivity1_ActorId1
+        {
+            get
+            {
+                return ((System.Guid)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_ActorId1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_ActorId1Property, value);
+            }
+        }
+
+        public static DependencyProperty updateResourceActivity1_ApplyAuthorizationPolicy1Property = DependencyProperty.Register("updateResourceActivity1_ApplyAuthorizationPolicy1", typeof(System.Boolean), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Boolean updateResourceActivity1_ApplyAuthorizationPolicy1
+        {
+            get
+            {
+                return ((bool)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_ApplyAuthorizationPolicy1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_ApplyAuthorizationPolicy1Property, value);
+            }
+        }
+
+        public static DependencyProperty updateResourceActivity1_ResourceId1Property = DependencyProperty.Register("updateResourceActivity1_ResourceId1", typeof(System.Guid), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Guid updateResourceActivity1_ResourceId1
+        {
+            get
+            {
+                return ((System.Guid)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_ResourceId1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_ResourceId1Property, value);
+            }
+        }
+
+        public static DependencyProperty updateResourceActivity1_UpdateParameters1Property = DependencyProperty.Register("updateResourceActivity1_UpdateParameters1", typeof(Microsoft.ResourceManagement.WebServices.WSResourceManagement.UpdateRequestParameter[]), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public UpdateRequestParameter[] updateResourceActivity1_UpdateParameters1
+        {
+            get
+            {
+                return ((Microsoft.ResourceManagement.WebServices.WSResourceManagement.UpdateRequestParameter[])(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_UpdateParameters1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_UpdateParameters1Property, value);
+            }
+        }
+
+        public static DependencyProperty updateResourceActivity1_AuthorizationWaitTimeInSeconds1Property = DependencyProperty.Register("updateResourceActivity1_AuthorizationWaitTimeInSeconds1", typeof(System.Int32), typeof(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration));
+
+        [DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility.Visible)]
+        [BrowsableAttribute(true)]
+        [CategoryAttribute("Parameters")]
+        public Int32 updateResourceActivity1_AuthorizationWaitTimeInSeconds1
+        {
+            get
+            {
+                return ((int)(base.GetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_AuthorizationWaitTimeInSeconds1Property)));
+            }
+            set
+            {
+                base.SetValue(FIM.DerivativeAttributesActivity.DerivativeAttributesGeneration.updateResourceActivity1_AuthorizationWaitTimeInSeconds1Property, value);
+            }
+        }
+        #endregion
+
+
+        public DerivativeAttributesGeneration()
+        {
+            InitializeComponent();
+        }
+
+        private void InitiliazeReadSubject_ExecuteCode(object sender, EventArgs e)
+        {
+            RequestType currentRequest = this.ReadCurrentRequestActivity.CurrentRequest;
+            ReadOnlyCollection<CreateRequestParameter> requestParameters = currentRequest.ParseParameters<CreateRequestParameter>();
+
+            SequentialWorkflow containingWorkflow = null;
+            if (!SequentialWorkflow.TryGetContainingWorkflow(this, out containingWorkflow))
+            {
+                throw new InvalidOperationException("Unable to get Containing Workflow");
+            }
+
+            this.readResourceActivity1_ActorId1 = new Guid(FIMADMGUID);
+            this.readResourceActivity1_ResourceId1 = containingWorkflow.TargetId;
+
+            requestorGUID = containingWorkflow.ActorId;
+            targetGUID = containingWorkflow.TargetId;
+        }
+
+        private void InitializeUpdate_ExecuteCode(object sender, EventArgs e)
+        {
+            //displayName = readResourceActivity1_Resource1["firstName"] + " " + readResourceActivity1_Resource1["lastName"];
+            displayName = readResourceActivity1.Resource["FirstName"].ToString() + " " + readResourceActivity1.Resource["LastName"].ToString();
+            updateResourceActivity1.UpdateParameters = new UpdateRequestParameter[] {
+                    new UpdateRequestParameter("DisplayName", UpdateMode.Modify, displayName)
+                };
+            updateResourceActivity1_ActorId1 = requestorGUID;
+            updateResourceActivity1_ResourceId1 = targetGUID;
+
+            //samAccountName 
+            string lastName = readResourceActivity1.Resource["LastName"].ToString();
+            string firstName = readResourceActivity1.Resource["FirstName"].ToString();
+            samAccountName = lastName.Length > 5 ? lastName.ToLower().Substring(0, 5) : lastName + firstName.ToLower().Substring(0,2) +"1";
+            updateResourceActivity1.UpdateParameters = new UpdateRequestParameter[]{
+                new UpdateRequestParameter("SamAccountName",UpdateMode.Insert,samAccountName)
+              };
+        }
+    }
+}
